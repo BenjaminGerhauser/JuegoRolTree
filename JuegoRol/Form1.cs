37df368bd3d41c1b2b31bd2013306a66421a859b
root <mgiraudo@ies21.edu.ar>
2024-04-19 12:56:53 -0300

    Agregar archivos de proyecto.

---
+++ JuegoRol/Form1.cs
@@ -0,0 +1,46 @@
+using MySql.Data.MySqlClient;
+namespace JuegoRol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TreeView treeViewX;
+
+            clsConexion con = new clsConexion();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.cargar(treeView1, "SELECT type FROM monstruario GROUP BY type");
+            //treeViewX = new TreeView();
+
+            //Controls.Add(treeViewX);
+
+            //// Crear un nodo raíz
+            //TreeNode nodoRaiz = treeViewX.Nodes.Add("Root Node");
+
+            //// Crear un nodo secundario
+            //TreeNode nodoHijo = nodoRaiz.Nodes.Add("Child Node");
+
+            //TreeNode nodoRaiz2 = treeViewX.Nodes.Add("hacker bordiga");
+
+            //TreeNode nodoHijo2 = nodoRaiz2.Nodes.Add("Child Node");
+            //TreeNode nodoHijo3 = nodoRaiz2.Nodes.Add("Child Node");
+
+            //TreeNode nodoHijo4 = nodoHijo3.Nodes.Add("Child Node");
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //MessageBox.Show("Nodo seleccionado: " + e.Node.Text);
+        }
+
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            con.caracteristicas(dataGridView1, e.Node.Text);
+
+        }
+    }
+}
---
+++ JuegoRol/clsConexion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace JuegoRol
+{
+    internal class clsConexion
+    {
+        string sql = "";
+        DataTable tabla;
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=juegorol;Uid=root;Pwd=;");
+        MySqlDataAdapter ada;
+        MySqlCommand com;
+
+        private DataTable consultar(string sql)
+        {
+            tabla = new DataTable();
+            ada = new MySqlDataAdapter(sql,con);
+            ada.Fill(tabla);
+            return tabla;
+        }
+        public void cargar(TreeView tree,string sql)
+        {
+            tree.Nodes.Clear();
+            tabla = consultar(sql);
+            TreeNode nodoRaiz = tree.Nodes.Add("Types");
+            foreach (DataRow r in tabla.Rows)
+            {
+                TreeNode nodoHijo = nodoRaiz.Nodes.Add(r["type"].ToString());
+                string type = r["type"].ToString();
+                nombre(nodoHijo, type);
+
+            }
+        }
+        private void nombre(TreeNode nodoPadre,string type)
+        {
+            string sql = $"SELECT name FROM monstruario WHERE type = '{type}'";
+            tabla = consultar(sql);
+            foreach (DataRow r in tabla.Rows)
+            {
+                nodoPadre.Nodes.Add(r["name"].ToString());
+                caract(nodoPadre, r["name"].ToString());
+            }
+
+        }
+        private void caract(TreeNode nodoPadre, string nombre)
+        {
+            string sql = $"SELECT * FROM monstruario WHERE name = '{nombre}'";
+            tabla = consultar(sql);
+            foreach (DataRow r in tabla.Rows)
+            {
+                nodoPadre.Nodes.Add(r["size"].ToString());
+                nodoPadre.Nodes.Add(r["hit_points"].ToString());
+                nodoPadre.Nodes.Add(r["strength"].ToString());
+                nodoPadre.Nodes.Add(r["dexterity"].ToString());
+                nodoPadre.Nodes.Add(r["constitution"].ToString());
+                nodoPadre.Nodes.Add(r["intelligence"].ToString());
+                nodoPadre.Nodes.Add(r["wisdom"].ToString());
+                nodoPadre.Nodes.Add(r["charisma"].ToString());
+                nodoPadre.Nodes.Add(r["xp"].ToString());
+            }
+            
+        }
+        public void caracteristicas(DataGridView grilla, string nombre)
+        {
+            string sql = $"SELECT * FROM monstruario WHERE name = '{nombre}'";
+            tabla = consultar(sql);
+            grilla.DataSource = tabla;
+        }
+    }
+}
